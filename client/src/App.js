0427e19422e2e6865a80f38a82f47e1ad0eb386c
AmandaCallender <amanda.callender@outlook.com>
2020-03-13 21:56:40 -0400

    Part

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/"></Route>
     </div>
   );
 };
